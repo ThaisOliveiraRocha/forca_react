03e7c3c8b435b2fc06d2c14760f9c4cd6eeb8bda
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-02-18 13:03:21 -0300

    Adicionando design e imagens

--- src/components/forca/Forca.js
+++ src/components/forca/Forca.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react'
 import { randomWord } from './palavras/frutas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import step0 from "./images/0.jpg"
+import step1 from "./images/1.jpg";
+import step2 from "./images/2.jpg";
+import step3 from "./images/3.jpg";
+import step4 from "./images/4.jpg";
+import step5 from "./images/5.jpg";
+import step6 from "./images/6.jpg";
 
 class Forca extends Component{
     constructor(props) {
@@ -19,6 +28,7 @@ class Forca extends Component{
 
     static defaultProps = {
         maxErros: 6,
+        images: [step0, step1, step2, step3, step4, step5, step6]
     }
 
     onChange(){
@@ -32,7 +42,7 @@ class Forca extends Component{
     }
 
     guessedWord(){
-        return this.state.resposta.split("").map(bingo => (this.state.palpite.has(bingo) ? bingo : " _ "))
+        return this.state.resposta.split("").map(bingo => (this.state.palpite.has(bingo) ? bingo : "_"))
     }
 
     handleGuess(event) {
@@ -58,29 +68,35 @@ class Forca extends Component{
 
     render(){
         const gameOver = this.state.erros >= this.props.maxErros
-        const altText = `${this.state.erros}/${this.props.maxErros} wrong guesses`
+        const altText = `${this.state.erros}/${this.props.maxErros} palpites errados`
         const venceu = this.guessedWord().join("") === this.state.resposta
         let teclado = this.generateButtons()
 
         if(venceu)
-            teclado = "YOU WON"
+            teclado = "VOCÊ VENCEU"
         if(gameOver)
-            teclado = "YOU LOST"
+            teclado = "VOCÊ PERDEU"
 
         return(
-            <div>
-                <h1> forca </h1>
-                <span className='navbar-text text-primary'>Palpites errados: {this.state.erros}</span>
-                <label>Digite o nome de uma fruta </label>
+            <div className='Hangman'>
+                <nav className='navbar navbar-light justify-content-between'>
+                    <p className='navbar-brand text-light'>
+                        Forca
+                    </p>
+                    <p className='navbar-brand text-light'>
+                        Palpites errados: {this.state.erros}
+                    </p>
+                </nav>
+                <p className='text-center'>
+					<img src={this.props.images[this.state.erros]} alt={altText} />
+				</p>
+                <p className='text-center text-light'>Adivinhe o nome da fruta</p>
+                <p className='Hangman-word text-center'> 
+                    {!gameOver ? this.guessedWord() : this.state.resposta}{" "} 
+                </p>
 
-                <p> {!gameOver ? this.guessedWord() : this.state.resposta}{" "} </p>
-                <p>{teclado}</p>
-
-                <p>Palavra sorteada: {this.state.resposta}</p>
-                <p>{altText}</p>
-                    
+                <p className='text-center text-warning mt-4'>{teclado}</p>
             </div>
-            
         );
     }
 }
