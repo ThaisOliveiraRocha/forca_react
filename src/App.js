48e22f7fe0319c15234a57f2465c435c495c3cd8
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-02-18 08:38:18 -0300

    lista itens

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import ListaItens from './components/ListaItens'
+
+class App extends Component {
+  render() {
+    return (
+      <ListaItens />
+    );
+  }
+}
+
+export default App;
